ce2d2d0ec2565ce34dbfae88fe2a284c806606c9
s4w3d0ff <s4w3d0ff@users.noreply.github.com>
2025-01-17 03:52:02 -0700

    Delete install_deez.bat

--- install_deez.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Installing necessary Python packages...
-pip install -r requirements.txt
-
-echo Installing spaCy model...
-python -m spacy download en_core_web_sm
-
-echo Installation complete!
-pause
