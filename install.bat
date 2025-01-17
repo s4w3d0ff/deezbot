b1e10ee126666bf47cf69920bb4eab9d72f824c9
s4w3d0ff <s4w3d0ff@users.noreply.github.com>
2025-01-17 03:51:20 -0700

    idk

---
+++ install.bat
@@ -0,0 +1,21 @@
+@echo off
+echo Creating virtual environment...
+python -m venv deez_venv
+
+echo Activating virtual environment...
+call deez_venv\Scripts\activate
+
+echo Updating pip...
+python -m pip install --upgrade pip
+
+echo Installing necessary Python packages...
+pip install --upgrade --force-reinstall -r requirements.txt
+
+echo Installing spaCy model...
+python -m spacy download en_core_web_sm
+
+echo Deactivating virtual environment...
+deactivate
+
+echo Installation complete!
+pause
