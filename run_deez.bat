a966ee2c6dc51b2ebc4b44f80225348bbd58cd61
s4w3d0ff <s4w3d0ff@users.noreply.github.com>
2025-01-17 02:29:13 -0700

    Update run_deez.bat
    time to change these

--- run_deez.bat
+++ run_deez.bat
@@ -1,7 +1,7 @@
 @echo off
 echo Setting environment variables...
-set DEEZ_CLIENT_ID=w5sy7hi4s13v8hud5kyym807xmpq3g
-set DEEZ_CLIENT_SECRET=i7p1eep97awk3qtc6eo9ip10chj2hc
+set DEEZ_CLIENT_ID=w
+set DEEZ_CLIENT_SECRET=
 echo Running deez_nuts.py...
 py deez_nuts.py
 pause
