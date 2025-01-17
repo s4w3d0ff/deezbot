f9910efc5463dc0c0aed397ec18a3d8cf8d94c0a
s4w3d0ff <s4w3d0ff@users.noreply.github.com>
2025-01-17 03:54:48 -0700

    Add files via upload

---
+++ run.bat
@@ -0,0 +1,14 @@
+@echo off
+echo Activating virtual environment...
+call deez_venv\Scripts\activate
+
+echo Setting environment variables...
+set DEEZ_CLIENT_ID=
+set DEEZ_CLIENT_SECRET=
+
+echo Running deez_nuts.py...
+python deez_nutz.py
+
+echo Deactivating virtual environment...
+deactivate
+pause
