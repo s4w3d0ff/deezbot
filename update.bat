fa79c3b13dabaaf36f186a4b11385f38c9553e4c
s4w3d0ff <s4w3d0ff@users.noreply.github.com>
2025-01-17 20:49:56 -0700

    Add files via upload

--- update.bat
+++ update.bat
@@ -1,6 +1,4 @@
 @echo off
-echo Creating virtual environment...
-python -m venv deez_venv
 
 echo Activating virtual environment...
 call deez_venv\Scripts\activate
@@ -8,14 +6,11 @@ call deez_venv\Scripts\activate
 echo Updating pip...
 python -m pip install --upgrade pip
 
-echo Installing necessary Python packages...
+echo Updating Python packages...
 pip install --upgrade -r requirements.txt
-
-echo Installing spaCy model...
-python -m spacy download en_core_web_sm
 
 echo Deactivating virtual environment...
 call deez_venv\Scripts\deactivate
 
-echo Installation complete!
+echo Update complete!
 pause
